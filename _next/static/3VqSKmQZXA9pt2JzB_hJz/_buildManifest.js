6752b0a8ce4ca726cb0acea68d766beb7dcd3a97
rizky <ario@riz.ky>
2022-10-17 18:43:39 +0000

    Deploying to public from @ retrovrs/signature-based-minting@53969d20ec0c00aa20bb9b78a9348c70d4819f86 🚀

---
+++ _next/static/3VqSKmQZXA9pt2JzB_hJz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/28c4b70f1dde7b1a.css","static/chunks/pages/index-0714e886af0ae5b9.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
